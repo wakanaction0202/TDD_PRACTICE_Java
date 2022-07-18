0b939c0cad9a41b00229f3ba087c9e51ff57bca7
wakana <greentea.mmlg.0405@gmail.com>
2022-07-18 18:15:05 +0900

    第一章_仮実装_完了

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,10 +1,11 @@
 package money;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
     Dollar(int amount) {
+        this.amount = amount;
     }
-    void times(int multiplier){
-
+    void times(int multiplier) {
+        amount *= multiplier;
     }
 }

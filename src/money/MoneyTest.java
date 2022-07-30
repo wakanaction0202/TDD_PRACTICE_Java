2596cb6f8a7b849b9e3b4e72e954b29c3384e631
wakana <greentea.mmlg.0405@gmail.com>
2022-07-30 19:40:27 +0900

    第11章_不要になったら消す

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -14,12 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
  * - [ ] nullとの比較
  * - [ ] 他のオブジェクトとの等価性比較
  * - [x] 5CHF * 2 = 10CHF
- * - [ ] DollarとFrancの重複
+ * - [x] DollarとFrancの重複
  * - [x] equalsの一般化
  * - [x] timesの一般化
  * - [x] Franc と Dollarを比較する
  * - [x] 通貨の概念
- * - [ ] testFrancMultiplicationを削除する？
+ * - [x] testFrancMultiplicationを削除する？
  */
 public class MoneyTest {
     @Test
@@ -32,26 +32,12 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
